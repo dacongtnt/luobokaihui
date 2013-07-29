f9e1b2d004b00fee4e4d7dfefc07d7603d468293
dacongtnt <1269111593@qq.com>
2013-07-29 08:58:31 +0800

    mahailong
    login

--- LoginController.m
+++ LoginController.m
@@ -14,6 +14,12 @@
 
 #import "LoginController.h"
 #import "LoginInfo.h"
+#import "JSONKit.h"
+#import "ViewController.h"
+#import "ASIHTTPRequest.h"
+#import "Login.h"
+#import "DemoViewController.h"
+#import "AudioView.h"
 
 @interface LoginController ()
 - (void)onLogin:(QButtonElement *)buttonElement;
@@ -53,20 +59,13 @@
 //    [alert show];
 //}
 
-- (void)onLogin:(QButtonElement *)buttonElement {
-    [self loading:YES];
-    LoginInfo *info = [[LoginInfo alloc] init];
-    [self.root fetchValueIntoObject:info];
 
-    [self performSelector:@selector(loginCompleted:) withObject:info afterDelay:2];
-}
-
-- (void)onAbout {
-    QRootElement *details = [LoginController createDetailsForm];
-
-    QuickDialogController *quickform = [QuickDialogController controllerForRoot:details];
-    [self presentModalViewController:[[UINavigationController alloc] initWithRootViewController:quickform] animated:YES];
-}
+//- (void)onAbout {
+//    QRootElement *details = [LoginController createDetailsForm];
+//
+//    QuickDialogController *quickform = [QuickDialogController controllerForRoot:details];
+//    [self presentModalViewController:[[UINavigationController alloc] initWithRootViewController:quickform] animated:YES];
+//}
 
 -(void) cell:(UITableViewCell *)cell willAppearForElement:(QElement *)element atIndexPath:(NSIndexPath *)indexPath{
     cell.backgroundColor = [UIColor colorWithRed:0.9582 green:0.9104 blue:0.7991 alpha:1.0000];
@@ -126,6 +125,116 @@
 
     return root;
 }
+
+//这里是我自己添加的方法
+- (void)loginCompleted:(LoginInfo *)info {
+    [self loading:NO];
+    NSLog(@"%@,%@",info.login,info.password);
+    if (([info.login isEqualToString:@""]||info.login==nil)||([info.password isEqualToString:@""]||info.password==nil)) {
+        UIAlertView *WarnAlert=[[UIAlertView alloc] initWithTitle:@"Warn" message:@"用户名或密码为空" delegate:self cancelButtonTitle:@"YES!" otherButtonTitles:nil, nil];
+        [WarnAlert show];
+        MCRelease(WarnAlert);
+    }else if(info.login.length<4||info.login.length>16){
+        UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"提示：" message:@"账户名必须在4～16位之间" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil];
+        [alert show];
+    }else if(info.password.length<4||info.password.length>16){
+        UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"提示：" message:@"密码必须在4～16位之间" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil];
+        [alert show];
+    }else{
+        //内网测试数据，为了保证程序的通用性，下面用假数据代替
+        //        NSURL *pasUrl = [NSURL URLWithString:[NSString stringWithFormat:@"http://172.16.2.16:8023/MeetingSystemss/LoginMeeting?username=%@&password=%@",info.login,info.password]];
+        //        ASIHTTPRequest *pasRequest = [ASIHTTPRequest requestWithURL:pasUrl];
+        //        [pasRequest startSynchronous];
+        //
+        //        NSError *loginError = [pasRequest error];
+        //        if(!loginError)
+        //        {
+        //            NSData *pasResponse = pasRequest.responseData;
+        //            NSDictionary *pasDic = [NSJSONSerialization JSONObjectWithData:pasResponse options:kNilOptions error:nil];
+        //            NSString *isYes = [pasDic objectForKey:@"flag"];
+        //            if ([isYes isEqualToString:@"YES"])
+        //            {
+        //                NSURL *conferenceUrl = [NSURL URLWithString:[NSString stringWithFormat:@"http://172.16.2.16:8023/MeetingSystemss/GetMeetingTitle?username=%@",info.login]];
+        //                ASIHTTPRequest *conferenceRequest = [ASIHTTPRequest requestWithURL:conferenceUrl];
+        //                [conferenceRequest startSynchronous];
+        //                NSError *conferenceError = [conferenceRequest error];
+        //                if (!conferenceError)
+        //                {
+        //                    NSData *conferenceResponse = conferenceRequest.responseData;
+        //                    NSDictionary *conferenceDic = [NSJSONSerialization JSONObjectWithData:conferenceResponse options:kNilOptions error:nil];
+        //                    NSArray *startArr = [conferenceDic objectForKey:@"start"];
+        //                    NSArray *endArr = [conferenceDic objectForKey:@"end"];
+        //                    NSMutableArray *startName = [[NSMutableArray alloc] initWithCapacity:0];
+        //                    NSMutableArray *endName = [[NSMutableArray alloc] initWithCapacity:0];
+        //                    for (int i = 0; i < startArr.count; i++) {
+        //                        [startName addObject:[[startArr objectAtIndex:i] objectForKey:@"Conference_title"]];
+        //                    }
+        //                    for (int i = 0; i < endArr.count; i++) {
+        //                        [endName addObject:[[endArr objectAtIndex:i] objectForKey:@"Conference_title"]];
+        //                    }
+        //                    ViewController *regist = [[ViewController alloc] init];
+        //                    [self.navigationController pushViewController:regist animated:YES];
+        //                }
+        //                else
+        //                {
+        //                    NSLog(@"%@",conferenceError);
+        //                }
+        //            }
+        
+        
+        
+        NSString *yonghuxinxi=@"{\"mahailong\":\"sunao\",\"majian\":\"sunao\",\"zhangjian\":\"sunao\",\"zhangwei\":\"sunao\",\"liuqingxuan\":\"sunao\"}";
+        NSData *data=[yonghuxinxi dataUsingEncoding:NSUTF8StringEncoding];
+        NSDictionary *dic=[data objectFromJSONData];
+        
+        NSString *strPass=[dic objectForKey:info.login];
+        NSLog(@"%@",strPass);
+        NSLog(@"%@",dic);
+        
+        if (strPass==nil||[strPass isEqualToString:@""]) {
+            UIAlertView *WarnAlertb=[[UIAlertView alloc] initWithTitle:@"Warn" message:@"用户名不存在" delegate:self cancelButtonTitle:@"YES!" otherButtonTitles:nil, nil];
+            [WarnAlertb show];
+            MCRelease(WarnAlertb);
+        }else{
+            if([strPass  isEqualToString:info.password]){
+                
+                //            QRootElement *root=[Login createMainFrom];
+                //            ViewController *view=[[ViewController alloc] initWithRoot:root];
+                
+                AudioView *view=[[AudioView alloc] init];
+                
+                [self.navigationController pushViewController:view animated:YES];
+                MCRelease(view);
+            }else {
+                UIAlertView *WarnAlertb=[[UIAlertView alloc] initWithTitle:@"Warn" message:@"密码错误" delegate:self cancelButtonTitle:@"YES!" otherButtonTitles:nil, nil];
+                [WarnAlertb show];
+                MCRelease(WarnAlertb);
+            }
+        }
+    }
+}
+
+//-(void)requestFinished:(ASIHTTPRequest*)request
+//{
+//    NSData *pasResponse = request.responseData;
+//
+//    NSLog(@"sss is %@",pasResponse);
+//    NSDictionary *pasDic =[pasResponse objectFromJSONData];
+//    NSLog(@"sss is %@",pasDic);
+//
+//
+//}
+
+//这里是我自己添加的方法
+- (void)onLogin:(QButtonElement *)buttonElement {
+    [self loading:YES];
+    LoginInfo *info = [[LoginInfo alloc] init];
+    
+    [self.root fetchValueIntoObject:info];
+    
+    [self performSelector:@selector(loginCompleted:) withObject:info afterDelay:2];
+}
+
 
 - (BOOL)QEntryShouldChangeCharactersInRangeForElement:(QEntryElement *)element andCell:(QEntryTableViewCell *)cell {
     NSLog(@"Should change characters");
